80acf147d0928b459ebe552235f8d2fb287e27cc
Jhonathan Alejandro <jhonathanalejandroa@gmail.com>
2021-11-20 18:05:54 -0500

    first commit

---
+++ app/src/main/java/com/virtual_android/a_donde_ir/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.virtual_android.a_donde_ir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
